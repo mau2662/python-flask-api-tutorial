7bfc2a44123a2e557f817d966f66cb0a2be89333
mau2662 <vargas7384@gmail.com>
2021-04-11 16:21:58 +0000

    listo

--- src/app.py
+++ src/app.py
@@ -18,6 +18,7 @@ def add_new_todo():
 
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
+    todos.pop(position)
     print("This is the position to delete: ",position)
     return jsonify(todos)
 
